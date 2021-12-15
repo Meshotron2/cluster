4591916e454d6e698ac3b8345d77bc673724ee75
Joao-Felisberto <joaofelisberto@ua.pt>
2021-12-15 15:05:10 +0000

    Skeleton done

--- cluster/cluster.c
+++ cluster/cluster.c
@@ -29,16 +29,69 @@ int main(int argc, char *argv[])
 	}
 
 	FILE *inFile = fopen(argv[1], "rb");
-	if (!inFile) {
+	if (!inFile) 
+	{
 		perror("fopen");
 		return EXIT_FAILURE;
 	}
 
 	Header h = { 0 };
 	fread(&h, sizeof(Header), 1, inFile);
+	
+	int node[h.x][h.y][h.z];
+	int hasSourcesReceivers = readNode(node, &h, inFile);
 
 	printf("%d, %d, %d @ %ld\n", h.x, h.y, h.z, h.frequency);
+
+	if (hasSourcesReceivers & 1 == 1)
+		readSample();
+
+	while (1) // ! lastIteration
+	{
+		if (hasSourcesReceivers & 1 == 1)
+			injectSample();
+
+		scatter();
+
+		if (hasSourcesReceivers & 2 == 2)
+			readSample(); // ------
+
+		delay();
+	}
+
+	if (hasSourcesReceivers & 2 == 2)
+		writeExcitation();
 
 	return EXIT_SUCCESS;
 }
 
+int readNode(int ***node, Header *h, FILE *inFile) 
+{
+	printf("ROOM: %d %d %d\n", h->x, h->y, h->z);
+
+	for (int x = 0; x < h->x; x++)
+		for (int y = 0; y < h->y; y++)
+			for (int z = 0; z < h->z; z++)
+			{
+				printf("%d %d %d\n", x, y, z);
+				fread(&(node[x][y][z]), 4, 1, inFile);
+			}
+	
+	for (int x = 0; x < h->x; x++)
+		for (int y = 0; y < h->y; y++)
+			for (int z = 0; z < h->z; z++)
+				printf("%d, ", node[x][y][z]);
+	printf("\n");
+
+	return 0; // should return 1 if sources are found
+}
+
+void readSample() {}
+
+void injectSample() {}
+
+void scatter() {}
+
+void delay() {}
+
+void writeExcitation() {}
--- cluster/cluster.h
+++ cluster/cluster.h
@@ -2,4 +2,17 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+
 #include"types.h"
+
+int readNode(int ***node, Header *h, FILE *inFile); 
+
+void injectSample();
+
+void readSample();
+
+void scatter();
+
+void delay();
+
+void writeExcitation();
